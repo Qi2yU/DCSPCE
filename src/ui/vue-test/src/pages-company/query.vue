bb4955efce70026af14c95a076b31d04eb5cb4e3
BITcyman <815207911@qq.com>
2023-10-26 11:02:46 +0800

    company upload employ Data

--- src/ui/vue-test/src/pages-company/query.vue
+++ src/ui/vue-test/src/pages-company/query.vue
@@ -1,13 +1,28 @@
-<style>
-</style>
+
 <template>
-  <div id="user">
-    <h1>对比分析</h1>
+  <div :xl="6" :lg="7" class="bg-query">
+    <el-row type="flex" class="row-bg row-two" justify="center" align="top">
+      <el-col :span="6"></el-col>
+      <el-col :span="10">
+        <!--标题-->
+        <h1 class="head">往期数据查询</h1>
+      </el-col>
+      <el-col :span="6"></el-col>
+    </el-row>
+  
+  
   </div>
+  
 </template>
 
 <script>
-export default {
-  name: 'User'
-}
+
 </script>
+
+
+<style lanng="less"> 
+.bg-query {
+    height: 100%;
+  }
+
+</style>
---
+++ src/ui/vue-test/src/pages-company/qurey_result.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div :xl="6" :lg="7" class="bg-query">
+      <el-row type="flex" class="row-bg row-two" justify="center" align="top">
+        <el-col :span="6"></el-col>
+        <el-col :span="10">
+          <!--标题-->
+          <h1 class="head">查询结果</h1>
+        </el-col>
+        <el-col :span="6"></el-col>
+      </el-row>
+    
+    
+    </div>
+    
+  </template>
+  
+  <script>
+  
+  </script>
+  
+  
+  <style lanng="less"> 
+  .bg-query {
+      height: 100%;
+    }
+  
+  </style>
