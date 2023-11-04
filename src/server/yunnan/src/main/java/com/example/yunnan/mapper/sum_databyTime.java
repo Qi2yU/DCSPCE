a081713b6bac8544cb619a6dac55b692189c7615
Minimus27 <2751983225@qq.com>
2023-11-04 21:06:35 +0800

    日期计算

--- src/server/yunnan/src/main/java/com/example/yunnan/mapper/sum_databyTime.java
+++ src/server/yunnan/src/main/java/com/example/yunnan/mapper/sum_databyTime.java
@@ -1,4 +1,13 @@
 package com.example.yunnan.mapper;
 
+import com.example.yunnan.entity.SumEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+@Mapper
 public interface sum_databyTime {
+
+
+    List<SumEntity> get_data(String timeTableId);
 }
--- src/server/yunnan/src/main/java/com/example/yunnan/service/sum_databyTime_service.java
+++ src/server/yunnan/src/main/java/com/example/yunnan/service/sum_databyTime_service.java
@@ -3,6 +3,7 @@ import com.example.yunnan.entity.SumEntity;
 import com.example.yunnan.mapper.sum_databyOthers;
 import com.example.yunnan.mapper.sum_databyTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,13 +15,46 @@ public class sum_databyTime_service {
     private List<String> User_idList;
     private List<String> Time_idList;
 
-    public void getUserId(String sum_id){
-        User_idList = sum_databyOthers_Mapper.get_user_id(sum_id);
+    String compute_Time(String time, String et){//字符串类型：20xx0901， 20xx0900，长度为8
+        if(Integer.valueOf(time) >= Integer.valueOf(et)){
+            return et;
+        }
+        else{
+            if(time.charAt(7) == '1'){
+                StringBuffer sb =  new StringBuffer(time);
+                char month_last = time.charAt(5);
+                char month_first = time.charAt(4);
+                if(month_last == '2' && month_first =='1'){
+                    sb.replace(7,7,String.valueOf('0'));
+                    sb.replace(4,5,"01");
+
+                }
+                else{
+                    if(month_last == '9'){
+                        sb.replace(4,5,"10");
+                        sb.replace(7,7,String.valueOf('0'));
+                        return sb.toString();
+
+                    }
+                    else {
+                        int ml_int = Integer.valueOf(String.valueOf(month_last));
+                        ml_int++;
+                        sb.replace(7,7,String.valueOf('0'));
+                        sb.replace(5,5,String.valueOf(ml_int));
+                        return sb.toString();
+                    }
+                }
+
+            }
+            else if(time.charAt(7) == '0'){
+                StringBuffer sb =  new StringBuffer(time);
+                return sb.replace(7,7,String.valueOf('1')).toString();
+            }
+        }
+        return et;
     }
-    public void getTimeId(String start_time, String end_time){
-        Time_idList = sum_databyOthers_Mapper.get_time_id(start_time, end_time);
-    }
-    public List<SumEntity> getResult(List<String> User_idList, List<String> Time_idList){
-        return  sum_databyOthers_Mapper.get_result(User_idList,Time_idList);
-    }
+
+
+
+
 }
